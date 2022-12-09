ae75585d9b958c56865bff64beab67c031bae063
Ansal Antony <>
2022-12-09 20:55:07 +0100

    -> Dental Care App created -> First commit with all files.

---
+++ DentalCare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DentalCare
+//
+//  Created by Ansal Antony on 09/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
