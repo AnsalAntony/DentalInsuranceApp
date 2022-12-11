6eb646e1c2f18677aaa25d152bf25aed89aa54ed
Ansal Antony <>
2022-12-11 02:51:21 +0100

    1 design completed for register chilled details. 2 changed title colors from black to white for getting better attraction for the app 4 used card components for designing the chilled details 5 used check box for identifying the child's gender 6 used the text view component for displaying text views 7 outlets created

---
+++ DentalCare/Controllers/ChildDetailViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ChildDetailViewController.swift
+//  DentalCare
+//
+//  Created by Ansal Antony on 10/12/22.
+//
+
+import UIKit
+
+class ChildDetailViewController: UIViewController {
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var postalTextField: UITextField!
+    @IBOutlet weak var streetTextField: UITextField!
+    @IBOutlet weak var houseNumberTextField: UITextField!
+    @IBOutlet weak var otherGenderCheckBox: CheckBox!
+    @IBOutlet weak var femailCheckBox: CheckBox!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var personalNumberTextField: UITextField!
+    @IBOutlet weak var cardView: CardView!
+    @IBOutlet weak var maleCheckBox: CheckBox!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+        setupUi()
+        
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        cardView.fadeIn()
+        cardView.isHidden = false
+       
+    }
+    private func setupUi(){
+        cardView.fadeOut()
+        cardView.isHidden = true // displaying animation
+        maleCheckBox.checkSelectionBtn.addTarget(self, action: #selector(maleButtonClicked), for: .touchUpInside)
+        femailCheckBox.checkSelectionBtn.addTarget(self, action: #selector(femaileButtonClicked), for: .touchUpInside)
+        otherGenderCheckBox.checkSelectionBtn.addTarget(self, action: #selector(otherGenderButtonClicked), for: .touchUpInside)
+    }
+    
+    private func setupNavigationBar(){
+        if #available(iOS 15, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            appearance.backgroundColor = UIColor.clear
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        }
+        
+        UINavigationBar.appearance().tintColor = .white
+        navigationItem.title = "Child Details"
+        
+    }
+    @objc func maleButtonClicked(sender: UIButton) {
+        maleCheckBox.isChecked = !maleCheckBox.isChecked
+        
+    }
+    @objc func femaileButtonClicked(sender: UIButton) {
+        femailCheckBox.isChecked = !femailCheckBox.isChecked
+        
+    }
+    @objc func otherGenderButtonClicked(sender: UIButton) {
+        otherGenderCheckBox.isChecked = !otherGenderCheckBox.isChecked
+        
+    }
+  
+    @IBAction func parentDetailClicked(_ sender: Any) {
+    }
+    
+}
