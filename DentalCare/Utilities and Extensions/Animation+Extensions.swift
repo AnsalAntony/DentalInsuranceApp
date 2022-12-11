667d04942ce512d499827bee153dfde5a68b7187
Ansal Antony <>
2022-12-11 02:45:38 +0100

    1 created a custom component for the check box that will help to use other view controllers. 2 changed animation duration because it was showing very delayed.

--- DentalCare/Utilities and Extensions/Animation+Extensions.swift
+++ DentalCare/Utilities and Extensions/Animation+Extensions.swift
@@ -9,13 +9,14 @@ import Foundation
 import UIKit
 
 
+// MARK: - extension for displaying amnimantion
 extension UIView {
-    func fadeIn(_ duration: TimeInterval = 1.0, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+    func fadeIn(_ duration: TimeInterval = 0.8, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
         UIView.animate(withDuration: duration, delay: delay, options: UIView.AnimationOptions.curveEaseIn, animations: {
             self.alpha = 1.0
         }, completion: completion)  }
     
-    func fadeOut(_ duration: TimeInterval = 1.0, delay: TimeInterval = 0.0, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
+    func fadeOut(_ duration: TimeInterval = 0.8, delay: TimeInterval = 0.0, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
         UIView.animate(withDuration: duration, delay: delay, options: UIView.AnimationOptions.curveEaseIn, animations: {
             self.alpha = 0.0
         }, completion: completion)
--- DentalCare/Utilities and Extensions/UIView+Extensions.swift
+++ DentalCare/Utilities and Extensions/UIView+Extensions.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 extension UIView {
-    
+    // MARK: - craeting shado for view
     func setBackgroundShadow(setColor: UIColor) {
         //MARK:- Shade a view
         self.layer.shadowOpacity = 0.5
@@ -17,5 +17,13 @@ extension UIView {
         self.layer.shadowRadius = 5
         self.layer.shadowColor = setColor.cgColor
         self.layer.masksToBounds = false
+    }
+    
+    // MARK: - loading nib for creating custome components.
+    func loadViewFromNib(nibName: String) -> UIView{
+        let view = Bundle.main.loadNibNamed(nibName, owner: self, options: nil)?[0] as! UIView
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.frame = self.bounds
+        return view
     }
 }
