c7491b0976262ab9f0c2fd5cc3b1e5c2933da056
Ansal Antony <>
2022-12-11 15:25:26 +0100

    1 Constant file added for store the commen string and integer values 2  added card view Constraints for the top and bottom to adjust the size of the card view in low-resolution devices. 3 created an extension for finding if the device has a notch display.

---
+++ DentalCare/Constants/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  DentalCare
+//
+//  Created by Ansal Antony on 11/12/22.
+//
+
+import Foundation
+import UIKit
+
+
+struct Constants {
+    
+    static let childDetails = "Child Details"
+    static let cardViewTopConstraint: CGFloat = 100
+    static let cardViewBottomConstraint: CGFloat = 50
+
+}
--- DentalCare/Utilities and Extensions/AlertView+Extensions.swift
+++ DentalCare/Utilities and Extensions/AlertView+Extensions.swift
@@ -17,3 +17,21 @@ extension UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
 }
+
+extension UIViewController {
+    open override func awakeFromNib() {
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    }
+}
+
+extension UIDevice {
+    /// Returns `true` if the device has a notch
+    var hasNotch: Bool {
+        guard #available(iOS 11.0, *), let window = UIApplication.shared.windows.filter({$0.isKeyWindow}).first else { return false }
+        if UIDevice.current.orientation.isPortrait {
+            return window.safeAreaInsets.top >= 44
+        } else {
+            return window.safeAreaInsets.left > 0 || window.safeAreaInsets.right > 0
+        }
+    }
+}
