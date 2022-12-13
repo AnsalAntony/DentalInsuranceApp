d4be9b46b89a8da801c4325ffcf971b693cf331a
Ansal Antony <>
2022-12-13 01:05:08 +0100

    1 bank detail text field validation. 2 created new outlets and managed the checkboxes in InsuranceClaimViewController 3 validation and date picker implantation for InsuranceClaimViewController 4 created new outlets and managed the checkboxes in the parent detailsView controller. 5 validations for parent detailsView controller

--- DentalCare/Controllers/BankDetailsViewController.swift
+++ DentalCare/Controllers/BankDetailsViewController.swift
@@ -14,6 +14,8 @@ class BankDetailsViewController: UIViewController {
     @IBOutlet weak var bankNameTextField: UITextField!
     @IBOutlet weak var accountTextField: UITextField!
     
+    private let bankDetailsViewModel = BankDetailsViewModel()
+    
     static func make() -> BankDetailsViewController {
         let viewController = UIStoryboard(name: "BankDetail", bundle: nil).instantiateViewController(withIdentifier: Constants.storyboardId.bankDetailsViewController) as! BankDetailsViewController
         return viewController
@@ -34,6 +36,19 @@ class BankDetailsViewController: UIViewController {
     }
 
     @IBAction func saveClicked(_ sender: Any) {
+        let checkBankDetails = bankDetailsViewModel.validateBankDetails(bankId: bankIdTextField.text ?? "", accountNumber: accountTextField.text ?? "", bankName: bankNameTextField.text ?? "")
+        
+        if(!checkBankDetails.status && checkBankDetails.message != ""){
+            alertPresent(title: "", message: checkBankDetails.message)
+        }else{
+            // save bank details to the data base or call the post api
+            // in the sucess block naviagte to the next view controller.
+            
+            let viewController = BankDetailsViewController.make()
+            navigationController?.pushViewController(viewController, animated: true)
+
+        }
+        
         let homeViewController = HomeViewController.make()
         guard let navigationController = navigationController else { return }
         navigationController.setViewControllers([homeViewController], animated: false)
--- DentalCare/Controllers/ParentDetailsViewController.swift
+++ DentalCare/Controllers/ParentDetailsViewController.swift
@@ -22,10 +22,11 @@ class ParentDetailsViewController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var personalNumberTextField: UITextField!
     @IBOutlet weak var cardView: CardView!
-    
     @IBOutlet weak var cardViewTopConstraint: NSLayoutConstraint!
-    
     @IBOutlet weak var cardViewBottomConstraint: NSLayoutConstraint!
+    
+    private let parentDetailsViewModel = ParentDetailsViewModel()
+    private var selectedReleationShip = ""
     
     static func make() -> ParentDetailsViewController {
         let viewController = UIStoryboard(name: "ParentDetail", bundle: nil).instantiateViewController(withIdentifier: Constants.storyboardId.parentDetailsViewController) as! ParentDetailsViewController
@@ -63,25 +64,51 @@ class ParentDetailsViewController: UIViewController {
     
     @objc func fatherClicked(sender: UIButton) {
         fatherCheckBox.isChecked = !fatherCheckBox.isChecked
-        
+        motherTextField.isChecked = false
+        guardianCheckBox.isChecked = false
+        otherCheckBox.isChecked = false
+        selectedReleationShip = parentDetailsViewModel.manageReletationShip(reletationShip: "Father", selection: fatherCheckBox.isChecked)
     }
     @objc func motherClicked(sender: UIButton) {
+        fatherCheckBox.isChecked = false
         motherTextField.isChecked = !motherTextField.isChecked
+        guardianCheckBox.isChecked = false
+        otherCheckBox.isChecked = false
+        selectedReleationShip = parentDetailsViewModel.manageReletationShip(reletationShip: "Mother", selection: motherTextField.isChecked)
         
     }
     @objc func guardianClicked(sender: UIButton) {
+        fatherCheckBox.isChecked = false
+        motherTextField.isChecked = false
         guardianCheckBox.isChecked = !guardianCheckBox.isChecked
+        otherCheckBox.isChecked = false
+        selectedReleationShip = parentDetailsViewModel.manageReletationShip(reletationShip: "Guardian", selection: guardianCheckBox.isChecked)
         
     }
     @objc func otherClicked(sender: UIButton) {
+        fatherCheckBox.isChecked = false
+        motherTextField.isChecked = false
+        guardianCheckBox.isChecked = false
         otherCheckBox.isChecked = !otherCheckBox.isChecked
-        
+        selectedReleationShip = parentDetailsViewModel.manageReletationShip(reletationShip: "Other", selection: otherCheckBox.isChecked)
     }
     
     
     @IBAction func saveClicked(_ sender: Any) {
-        let viewController = BankDetailsViewController.make()
-        navigationController?.pushViewController(viewController, animated: true)
+        
+        let validateParentDetails = parentDetailsViewModel.parentDetails(personalNumber: personalNumberTextField.text ?? "", name: nameTextField.text ?? "", email: emailAddressTextField.text ?? "", mobile: mobileNumberTextField.text ?? "", releation: selectedReleationShip, house: houseTextField.text ?? "", street: streetTextField.text ?? "", post: postalcodeTextField.text ?? "", city: cityTextField.text ?? "")
+        
+        
+        if(!validateParentDetails.status && validateParentDetails.message != ""){
+            alertPresent(title: "", message: validateParentDetails.message)
+        }else{
+            // save parent details to the data base or call the post api
+            // in the sucess block naviagte to the next view controller.
+            
+            let viewController = BankDetailsViewController.make()
+            navigationController?.pushViewController(viewController, animated: true)
+
+        }
     }
     
     
